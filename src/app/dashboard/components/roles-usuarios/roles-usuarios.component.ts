8bd3686161b2890b6f701e5d6ab717fcd28825cd
juli.jimenez <juli.jimenez@duocuc.cl>
2022-09-13 00:17:32 -0300

    Botones de roles

--- src/app/dashboard/components/roles-usuarios/roles-usuarios.component.ts
+++ src/app/dashboard/components/roles-usuarios/roles-usuarios.component.ts
@@ -9,8 +9,34 @@ import { UsuariosService } from '../../services/usuarios.service';
 })
 export class RolesUsuariosComponent implements OnInit {
   roles: Rol[] = [];
+  esAdmin: boolean = false;
+  esMesero: boolean = false;
+  esCocinero: boolean = false;
+  esFinanzas: boolean = false;
+  esBodeguero: boolean = false;
 
-  constructor(private servicio: UsuariosService) {}
+  user: any = localStorage.getItem('usuario');
+  usuario = JSON.parse(this.user);
+
+  constructor(private servicio: UsuariosService) {
+    if (localStorage.getItem('token')) {
+      if (this.usuario.rolArray.find((e: any) => e === 1)) {
+        this.esAdmin = true;
+      }
+      if (this.usuario.rolArray.find((e: any) => e === 2)) {
+        this.esMesero = true;
+      }
+      if (this.usuario.rolArray.find((e: any) => e === 3)) {
+        this.esCocinero = true;
+      }
+      if (this.usuario.rolArray.find((e: any) => e === 4)) {
+        this.esFinanzas = true;
+      }
+      if (this.usuario.rolArray.find((e: any) => e === 5)) {
+        this.esBodeguero = true;
+      }
+    }
+  }
 
   ngOnInit(): void {
     this.obtenerRolesUsuarios();
