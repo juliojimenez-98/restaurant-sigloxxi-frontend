7738675316f2bb1321c8e48ae1dfbcc890e1da71
julio jimenez <juli.jimenez@duocucl.cl>
2022-09-14 11:23:07 -0300

    Guard para controlar vista del dashboard de que este logueado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ValidarTokenGuard } from './guard/validar-token.guard';
 
 const routes: Routes = [
   {
@@ -10,6 +11,7 @@ const routes: Routes = [
     path: 'admin',
     loadChildren: () =>
       import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
+    canActivate: [ValidarTokenGuard],
   },
   {
     path: 'home',
