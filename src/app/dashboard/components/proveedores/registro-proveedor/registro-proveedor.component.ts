44acbfc48dae7245e2b5d22035357c1e1d959aa2
Sofia <sofiapincheira.guzman@gmail.com>
2022-10-14 18:17:48 -0300

    Registro y lista de proveedores no completado

--- src/app/dashboard/components/proveedores/registro-proveedor/registro-proveedor.component.ts
+++ src/app/dashboard/components/proveedores/registro-proveedor/registro-proveedor.component.ts
@@ -1,15 +1,115 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import Swal from 'sweetalert2';
+import { ProveedoresService } from '../../../services/proveedores.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-registro-proveedor',
   templateUrl: './registro-proveedor.component.html',
-  styleUrls: ['./registro-proveedor.component.css']
+  styleUrls: ['./registro-proveedor.component.css'],
 })
 export class RegistroProveedorComponent implements OnInit {
+  formRegistroProveedor: FormGroup = this.fb.group({
+    id_proveedor: [],
+    nombre: ['', [Validators.required]],
+    tel_contacto: ['', [Validators.required]],
+    email: ['', [Validators.required]],
+    sitio_web: ['', [Validators.required]],
+  });
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private activatedRoute: ActivatedRoute,
+    private servicioPro: ProveedoresService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.obtenerProveedorPorId();
   }
 
+  registrarProveedor() {
+    Swal.fire({
+      allowOutsideClick: false,
+      icon: 'info',
+      text: 'Registrando Proveedor',
+      showConfirmButton: false,
+    });
+    this.servicioPro
+      .registroProveedor(this.formRegistroProveedor.value)
+      .subscribe(
+        (res: any) => {
+          Swal.close();
+          if (res.msg === 'ok') {
+            Swal.fire(
+              'Proveedor Registrado',
+              `El Proveedor ${res.proveedor.nombre} fue registrado exitosamente`,
+              'success'
+            );
+            console.log(res);
+          }
+        },
+        (error) => {
+          Swal.close();
+          console.log(error);
+          if (error.error.errors) {
+            Swal.fire(
+              'Error al registrar proveedor',
+              `${error.error.errors[0].msg} `,
+              'error'
+            );
+          } else {
+            Swal.fire(
+              'Error al registrar proveedor',
+              `${error.error.msg} `,
+              'error'
+            );
+          }
+        }
+      );
+  }
+
+  obtenerProveedorPorId() {
+    this.activatedRoute.params.subscribe((params) => {
+      let id = params['id'];
+      let id_proveedor = parseInt(id);
+      if (id_proveedor) {
+        this.servicioPro
+          .obtenerProveedorPorId(id_proveedor)
+          .subscribe((res: any) => {
+            console.log(res);
+
+            this.formRegistroProveedor.patchValue({
+              id_proveedor: res.findProveedor.id_proveedor,
+              nombre: res.findProveedor.nombre,
+              tel_contacto: res.findProveedor.tel_contacto,
+              email: res.findProveedor.email,
+              sitio_web: res.findProveedor.sitio_web,
+            });
+            console.log(res);
+            console.log(this.formRegistroProveedor.value.id_proveedor);
+          });
+      }
+    });
+  }
+
+  actualizarProveedor() {
+    this.activatedRoute.params.subscribe((params) => {
+      let id = params['id'];
+      let id_proveedor = parseInt(id);
+      this.servicioPro
+        .actualizarProveedor(this.formRegistroProveedor.value, id_proveedor)
+        .subscribe((res) => {
+          console.log('respuesta', res);
+          Swal.fire(
+            'Proveedor Actualizado',
+            `Proveedor actualizado con exito`,
+            'success'
+          );
+          this.router.navigateByUrl('/admin/proveedores/lista-proveedores');
+        });
+    });
+  }
 }
---
+++ src/app/dashboard/services/proveedores.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { Proveedor } from '../interfaces/proveedor.interface';
+import { Observable, map } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProveedoresService {
+  constructor(private http: HttpClient) {}
+
+  registroProveedor(proveedor: Proveedor): Observable<Proveedor> {
+    const urlRegistroProveedor =
+      'http://localhost:8080/api/proveedores/proveedor';
+
+    return this.http.post<Proveedor>(urlRegistroProveedor, proveedor);
+  }
+
+  obtenerProveedor(): Observable<any> {
+    const urlGetProveedores =
+      'http://localhost:8080/api/proveedores/proveedores';
+    return this.http
+      .get(urlGetProveedores)
+      .pipe(map((res: any) => res.proveedor as Proveedor[]));
+  }
+
+  obtenerProveedorPorId(id: any): Observable<any> {
+    const urlGetProveedor = `http://localhost:8080/api/proveedores/proveedor/${id}`;
+    return this.http.get<any>(urlGetProveedor);
+  }
+
+  actualizarProveedor(proveedor: Proveedor, id: number): Observable<Proveedor> {
+    const urlActualizarProveedor = `http://localhost:8080/api/proveedores/proveedor/${id}`;
+
+    return this.http.put<Proveedor>(urlActualizarProveedor, proveedor);
+  }
+
+  eliminarProveedor(id: number): Observable<any> {
+    const urlBorrarProveedor = `http://localhost:8080/api/proveedores/proveedor/${id}`;
+    return this.http.delete<any>(urlBorrarProveedor);
+  }
+}
