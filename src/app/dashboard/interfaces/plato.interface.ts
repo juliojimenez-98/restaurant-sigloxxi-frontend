c35e81b2c8b0346911f98bcfc016be76c3dc6594
julio jimenez <juli.jimenez@duocucl.cl>
2022-10-17 10:04:09 -0300

    Platos cards (no completo)

--- src/app/dashboard/interfaces/plato.interface.ts
+++ src/app/dashboard/interfaces/plato.interface.ts
@@ -1,8 +1,9 @@
 import { Receta } from './receta.interface';
 export interface Plato {
-  id_plato: number;
+  id_plato?: number;
   desc: number;
   precio: number;
+  img?: string;
   estado: number;
   id_receta: number;
   receta: Receta;
