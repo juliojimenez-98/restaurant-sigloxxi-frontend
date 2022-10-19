21e1e43c4f4e21e0ab90d7d51b71dd0ea16eb5a6
julio jimenez <juli.jimenez@duocucl.cl>
2022-10-19 15:12:54 -0300

    Reservando mesas desde el home

--- src/app/home-page/components/reserva-next-step/reserva-next-step.component.ts
+++ src/app/home-page/components/reserva-next-step/reserva-next-step.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReservarService } from '../../services/reservar.service';
 
 @Component({
   selector: 'app-reserva-next-step',
@@ -8,9 +9,12 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class ReservaNextStepComponent implements OnInit {
   fechaReserva: string = '';
+  email: any = '';
+  cel: any = '';
+  idCliente: number = 0;
   formRegistroReserva: FormGroup = this.fb.group({
     id_reserva: [],
-    cant_personas: ['', [Validators.required]],
+    cant_personas: [0, [Validators.required]],
     fecha_reserva: ['', [Validators.required]],
     hora_reserva: ['', [Validators.required]],
     id_cliente: ['', [Validators.required]],
@@ -23,15 +27,38 @@ export class ReservaNextStepComponent implements OnInit {
     { valor: 10, nombre: '10 Personas' },
     { valor: 12, nombre: '12 Personas' },
   ];
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private service: ReservarService) {
+    this.email = localStorage.getItem('email');
+    this.cel = localStorage.getItem('cel');
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.obtenerClienteParaReserva();
+  }
+
+  obtenerClienteParaReserva() {
+    this.service
+      .obtenerClienteParaReserva(this.email, parseInt(this.cel))
+      .subscribe((res: any) => {
+        this.idCliente = res.findCliente.id_cliente;
+        console.log(this.idCliente);
+      });
+  }
 
   OnDateChange(event: any) {
-    this.fechaReserva = `${event.date}-${event.month}-${event.year}`;
+    this.fechaReserva = `${event.year}-${event.month}-${event.date}`;
   }
 
   registroReserva() {
     this.formRegistroReserva.value.fecha_reserva = this.fechaReserva;
+    this.formRegistroReserva.value.id_cliente = this.idCliente;
+    this.formRegistroReserva.value.cant_personas = parseInt(
+      this.formRegistroReserva.value.cant_personas
+    );
+    this.service
+      .registroReserva(this.formRegistroReserva.value)
+      .subscribe((res: any) => {
+        console.log(res);
+      });
   }
 }
