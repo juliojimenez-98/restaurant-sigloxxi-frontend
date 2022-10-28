814a18b10685ebc73b09d4fbf63ee91ea2b60245
Sofia <sofiapincheira.guzman@gmail.com>
2022-10-28 16:20:00 -0300

    Avance Pedido Ingredientes

--- src/app/dashboard/components/registrar-pedidos-ingredientes/registrar-pedidos-ingredientes.component.ts
+++ src/app/dashboard/components/registrar-pedidos-ingredientes/registrar-pedidos-ingredientes.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-registrar-pedidos-ingredientes',
   templateUrl: './registrar-pedidos-ingredientes.component.html',
-  styleUrls: ['./registrar-pedidos-ingredientes.component.css']
+  styleUrls: ['./registrar-pedidos-ingredientes.component.css'],
 })
 export class RegistrarPedidosIngredientesComponent implements OnInit {
+  formRegistroPedidoIng: FormGroup = this.fb.group({
+    id_pedido: [],
+    fecha_despacho: ['', [Validators.required]],
+    cantidad: ['', [Validators.required]],
+    precio: ['', [Validators.required]],
+    estado: ['', [Validators.required]],
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
